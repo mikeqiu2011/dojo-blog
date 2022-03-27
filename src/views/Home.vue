571f693affcffb165bfbca928b003d6b33965c97
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-27 14:14:41 +0800

    computed value in setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,20 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h3>using refs</h3>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }} - {{ nameOne }}</p>
-    <button @click="updateNinjaOne">update</button>
-
-    <h3>using reactive</h3>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
-    <button @click="updateNinjaTwo">update</button>
+    {{ name }}
   </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref, computed } from "vue";
 export default {
   name: "Home",
   setup() {
-    const ninjaOne = ref({ name: "mario", age: 30 });
-    const ninjaTwo = reactive({ name: "mike", age: 20 });
+    const name = computed(() => {
+      return "mike";
+    });
 
-    const nameOne = ref("refName");
-    const nameTwo = reactive("reactiveName");
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40;
-      nameOne.value = "mike";
-    };
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 40; //we dont need to use value, access the property directly
-      nameTwo = "mike";
-    };
-
-    return {
-      ninjaOne,
-      ninjaTwo,
-      updateNinjaOne,
-      updateNinjaTwo,
-      nameOne,
-      nameTwo,
-    };
+    return { name };
   },
 };
 </script>
