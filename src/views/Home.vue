47e5e566154aef79bc1ddd25f25b751b79abca34
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-27 10:51:17 +0800

    template reference

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="home">
-    <h1>Home2</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <h1>Home</h1>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "Home",
   setup() {
     console.log("setup");
+    console.log(this); //'this' is null in setup
+
+    const p = ref(null);
 
     let name = "mario";
     let age = 30;
 
     const handleClick = (e) => {
-      console.log("you clicked me");
+      console.log(p);
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
   mounted() {
     console.log("mounted");
