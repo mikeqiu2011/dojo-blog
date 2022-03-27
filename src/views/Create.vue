29f2ba8b927e9ada7e16eb48a641c784b791c24b
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-27 21:58:44 +0800

    prevent duplicate tag

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -7,6 +7,7 @@
       <textarea v-model="body" required></textarea>
       <label>Tags (hit enter to enter a tag)</label>
       <input type="text" v-model="tag" @keydown.enter.prevent="addTag" />
+      <div v-for="tag in tags" :key="tag" class="pill">#{{ tag }}</div>
       <button>create</button>
     </form>
   </div>
@@ -34,11 +35,12 @@ export default {
     };
 
     const addTag = (e) => {
-      if (tag.value) {
-        console.log(tags.value);
+      tag.value = tag.value.trim();
+      if (tag.value && !tags.value.includes(tag.value)) {
+        console.log(tag.value);
         tags.value.push(tag.value);
-        tag.value = "";
       }
+      tag.value = "";
     };
 
     return { title, body, tag, tags, create, addTag };
