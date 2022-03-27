39464788528ddba3a21e4a5bc838403b99c8bf78
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-27 11:57:25 +0800

    using reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,10 @@
     <h3>using refs</h3>
     <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
     <button @click="updateNinjaOne">update</button>
+
+    <h3>using reactive</h3>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">update</button>
   </div>
 </template>
 
@@ -13,12 +17,17 @@ export default {
   name: "Home",
   setup() {
     const ninjaOne = ref({ name: "mario", age: 30 });
+    const ninjaTwo = reactive({ name: "mike", age: 20 });
 
     const updateNinjaOne = () => {
       ninjaOne.value.age = 40;
     };
 
-    return { ninjaOne, updateNinjaOne };
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 40; //we dont need to use value, access the property directly
+    };
+
+    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo };
   },
 };
 </script>
