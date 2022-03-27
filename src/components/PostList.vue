3ca5e8059e6e3527aef9132e00350272114077b3
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-27 15:30:45 +0800

    component nesting

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="post-list">
     <div v-for="post in posts" :key="post.id">
-      <h3>{{ post.title }}</h3>
-      <p>{{ post.body }}</p>
+      <SinglePost :post="post" />
     </div>
   </div>
 </template>
 
 <script>
+import SinglePost from "./SinglePost.vue";
 export default {
   props: ["posts"],
+  components: { SinglePost },
   setup(props) {
     console.log(props.posts);
   },
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="post">
+    <h3>{{ post.title }}</h3>
+    <p>{{ post.body }}</p>
+    <p>this is in single post</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+};
+</script>
+
+<style>
+</style>
