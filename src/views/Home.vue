72b479c4477c10d21baf6dc620881e6fd1d26756
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-27 14:09:51 +0800

    reactive not work with primitive type

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,11 @@
   <div class="home">
     <h1>Home</h1>
     <h3>using refs</h3>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }} - {{ nameOne }}</p>
     <button @click="updateNinjaOne">update</button>
 
     <h3>using reactive</h3>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
     <button @click="updateNinjaTwo">update</button>
   </div>
 </template>
@@ -19,15 +19,27 @@ export default {
     const ninjaOne = ref({ name: "mario", age: 30 });
     const ninjaTwo = reactive({ name: "mike", age: 20 });
 
+    const nameOne = ref("refName");
+    const nameTwo = reactive("reactiveName");
+
     const updateNinjaOne = () => {
       ninjaOne.value.age = 40;
+      nameOne.value = "mike";
     };
 
     const updateNinjaTwo = () => {
       ninjaTwo.age = 40; //we dont need to use value, access the property directly
+      nameTwo = "mike";
     };
 
-    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo };
+    return {
+      ninjaOne,
+      ninjaTwo,
+      updateNinjaOne,
+      updateNinjaTwo,
+      nameOne,
+      nameTwo,
+    };
   },
 };
 </script>
