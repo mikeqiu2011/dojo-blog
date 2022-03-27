3fb7ce606435dabb9798c88e8bf517d715562985
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-27 15:46:26 +0800

    snippet using computed value

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,8 +12,16 @@ import PostList from "../components/PostList.vue";
 export default {
   setup() {
     const posts = ref([
-      { title: "welcome to the blog", body: "test1", id: 1 },
-      { title: "top 5 css tips", body: "test2", id: 2 },
+      {
+        title: "welcome to the blog",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius blanditiis sunt quo voluptatum animi iure deserunt obcaecati, molestiae corrupti labore autem dignissimos. Facilis iusto cum dolorum, illo ullam aperiam deleniti quis unde iure dolores, sint atque distinctio maxime repudiandae praesentium tempora illum. Voluptatem architecto fugiat sit odio atque ipsam maiores assumenda enim pariatur, mollitia debitis. Optio deleniti eius, sequi commodi nesciunt id laborum ab architecto mollitia magni quos autem totam unde, in distinctio voluptates excepturi, veniam reprehenderit pariatur iusto alias soluta error eum! Maxime quae nihil fuga, porro nam obcaecati quidem, accusantium tenetur similique, voluptate ut laudantium magnam! Officia, reprehenderit!",
+        id: 1,
+      },
+      {
+        title: "top 5 css tips",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius blanditiis sunt quo voluptatum animi iure deserunt obcaecati, molestiae corrupti labore autem dignissimos. Facilis iusto cum dolorum, illo ullam aperiam deleniti quis unde iure dolores, sint atque distinctio maxime repudiandae praesentium tempora illum. Voluptatem architecto fugiat sit odio atque ipsam maiores assumenda enim pariatur, mollitia debitis. Optio deleniti eius, sequi commodi nesciunt id laborum ab architecto mollitia magni quos autem totam unde, in distinctio voluptates excepturi, veniam reprehenderit pariatur iusto alias soluta error eum! Maxime quae nihil fuga, porro nam obcaecati quidem, accusantium tenetur similique, voluptate ut laudantium magnam! Officia, reprehenderit!",
+        id: 2,
+      },
     ]);
 
     return { posts };
