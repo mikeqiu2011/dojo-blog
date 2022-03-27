9866664b051ed0ebbb1697365c1235af0d42d86e
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-27 16:49:40 +0800

    load data using async

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPost" :posts="posts" />
-    <button @click="showPost = !showPost">toggle posts</button>
-    <button @click="posts.pop()">delete a post</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
@@ -13,21 +11,29 @@ import PostList from "../components/PostList.vue";
 
 export default {
   setup() {
-    const posts = ref([
-      {
-        title: "welcome to the blog",
-        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius blanditiis sunt quo voluptatum animi iure deserunt obcaecati, molestiae corrupti labore autem dignissimos. Facilis iusto cum dolorum, illo ullam aperiam deleniti quis unde iure dolores, sint atque distinctio maxime repudiandae praesentium tempora illum. Voluptatem architecto fugiat sit odio atque ipsam maiores assumenda enim pariatur, mollitia debitis. Optio deleniti eius, sequi commodi nesciunt id laborum ab architecto mollitia magni quos autem totam unde, in distinctio voluptates excepturi, veniam reprehenderit pariatur iusto alias soluta error eum! Maxime quae nihil fuga, porro nam obcaecati quidem, accusantium tenetur similique, voluptate ut laudantium magnam! Officia, reprehenderit!",
-        id: 1,
-      },
-      {
-        title: "top 5 css tips",
-        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius blanditiis sunt quo voluptatum animi iure deserunt obcaecati, molestiae corrupti labore autem dignissimos. Facilis iusto cum dolorum, illo ullam aperiam deleniti quis unde iure dolores, sint atque distinctio maxime repudiandae praesentium tempora illum. Voluptatem architecto fugiat sit odio atque ipsam maiores assumenda enim pariatur, mollitia debitis. Optio deleniti eius, sequi commodi nesciunt id laborum ab architecto mollitia magni quos autem totam unde, in distinctio voluptates excepturi, veniam reprehenderit pariatur iusto alias soluta error eum! Maxime quae nihil fuga, porro nam obcaecati quidem, accusantium tenetur similique, voluptate ut laudantium magnam! Officia, reprehenderit!",
-        id: 2,
-      },
-    ]);
-    const showPost = ref(true);
+    const posts = ref([]);
+    const error = ref(null);
+    const uri = "http://localhost:3000/posts";
 
-    return { posts, showPost };
+    const load = async () => {
+      try {
+        let data = await fetch(uri);
+        // console.log(data);
+
+        if (!data.ok) {
+          throw Error("no data available");
+        }
+        posts.value = await data.json();
+        // console.log(posts.value);
+      } catch (err) {
+        console.log(err);
+        error.value = err.message;
+      }
+    };
+
+    load();
+
+    return { posts };
   },
   components: { PostList },
 };
