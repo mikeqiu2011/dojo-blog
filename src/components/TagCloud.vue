531d8e46e44e15cc5ee7f42c9f84f2462f170777
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-28 22:25:51 +0800

    add routing when click tag

--- src/components/TagCloud.vue
+++ src/components/TagCloud.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="tag-cloud">
     <h3>Tags</h3>
-    <div>{{ tags }}</div>
+    <div v-for="tag in tags" :key="tag">
+      <router-link :to="{ name: 'Tag', params: { tag: tag } }">{{
+        tag
+      }}</router-link>
+    </div>
   </div>
 </template>
 
