eb7cf5701418786ac8fd03421fa4fcece55daec1
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-28 21:42:12 +0800

    using Postlist instead

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -3,9 +3,7 @@
     <h1>tags</h1>
     <!-- <div v-if="error">error</div> -->
     <div v-if="tagPosts.length">
-      <div v-for="post in tagPosts" :key="post.id">
-        <SinglePost :post="post" />
-      </div>
+      <PostList :posts="tagPosts" />
     </div>
     <div v-else>
       <Spinner />
@@ -16,7 +14,7 @@
 <script>
 import getPosts from "../composables/getPosts.js";
 import Spinner from "../components/Spinner.vue";
-import SinglePost from "../components/SinglePost.vue";
+import PostList from "../components/PostList.vue";
 
 import { useRoute } from "vue-router";
 import { computed } from "@vue/runtime-core";
@@ -25,24 +23,16 @@ export default {
   setup() {
     const { posts, error } = getPosts();
     const route = useRoute();
-    // console.log(posts.value);
-    console.log(error);
-
-    // console.log(route.params.tag);
 
     const tagPosts = computed(() => {
       return posts.value.filter((post) => {
         return post.tags.includes(route.params.tag);
       });
     });
-    // const { posts, error } = getPosts();
-    // console.log(posts.value);
-
-    // return { posts, error };
 
     return { tagPosts, error };
   },
-  components: { Spinner, SinglePost },
+  components: { Spinner, PostList },
 };
 </script>
 
