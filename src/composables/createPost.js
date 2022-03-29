4e619e7bb6d79b39e08a19c433de8d10df8f2fc8
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-29 20:09:43 +0800

    add a post to firestore

--- src/composables/createPost.js
+++ src/composables/createPost.js
@@ -1,28 +1,17 @@
 
 import { ref } from "vue"
+import db from "../firebase/config";
 
 const createPost = (post) => {
 
     const create = async () => {
         try {
-            //simulate delay
-            // await new Promise(resolve => { setTimeout(resolve, 2000) })
-
-            let resp = await fetch("http://localhost:3000/posts/", {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(post)
-            });
-            // console.log(data);
-
-            if (!resp.ok) {
-                throw Error("post failed");
-            }
-
-            // console.log(posts.value);
+            console.log(post);
+            const res = await db.collection('posts').add(post)
+            // console.log(res);
         } catch (err) {
             console.log(err);
-            error.value = err.message;
+            // error.value = err.message;
         }
     };
 
