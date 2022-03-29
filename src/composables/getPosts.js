11be36251ee07b010c416700e5c94bdc7fe6365b
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-29 17:59:42 +0800

    now also can get id property

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -8,7 +8,15 @@ const getPosts = () => {
     const load = async () => {
         try {
             const res = await projectFirestore.collection('posts').get()
-            posts.value = res.docs.map(doc => doc.data())
+            posts.value = res.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    title: doc.data().title,
+                    body: doc.data().body,
+                    tags: doc.data().tags
+                }
+            })
+            console.log(posts.value);
 
         } catch (err) {
             console.log(err);
