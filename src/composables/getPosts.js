394b9b9e730daf29ed8bd3aaffd037de99fdbd03
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-29 13:58:17 +0800

    firebase init

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -6,8 +6,6 @@ const getPosts = () => {
 
     const load = async () => {
         try {
-            //simulate delay
-            await new Promise(resolve => { setTimeout(resolve, 1000) })
 
             let data = await fetch("http://localhost:3000/posts");
             // console.log(data);
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA9-vXIwCXj-nG7ioC61LOEgYLB_Fymzlo",
+    authDomain: "udemy-vue-firebase-sites-a263f.firebaseapp.com",
+    projectId: "udemy-vue-firebase-sites-a263f",
+    storageBucket: "udemy-vue-firebase-sites-a263f.appspot.com",
+    messagingSenderId: "419966689121",
+    appId: "1:419966689121:web:6aeadbab12a2a35f87ef20"
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+//init firestore service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
